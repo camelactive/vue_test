e4d85f716ab25c1bc449f2bf784776ca5b71d094
camelactive <79437316+camelactive@users.noreply.github.com>
2022-05-27 00:06:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,46 +4,92 @@ var App = new Vue({
         title: 'toDoListApp',
         ButtonText: 'Add new Task',
         newTask: "",
-        tasks: JSON.parse(localStorage.getItem("memory")) || [],
-
+        tasksDones: JSON.parse(localStorage.getItem("done")) ? JSON.parse(localStorage.getItem("done")) : [],
+        tasks: JSON.parse(localStorage.getItem("memory")) ? JSON.parse(localStorage.getItem("memory")) : [],
+        checked: "",
+        icons: ["-", "+"],
     },
     methods: {
         newTaskClick: () => {
-            App.tasks.push(App.newTask);
-            App.newTask = "";
-            localStorage.setItem("memory", JSON.stringify(App.tasks))
+            if (App.newTask != "") {
+                App.tasks.push(App.newTask);
+                App.newTask = "";
+                localStorage.setItem("memory", JSON.stringify(App.tasks));
+                App.tasksDones.push(App.icons[0]);
+                localStorage.setItem("done", JSON.stringify(App.tasksDones));
+            }
         },
         taskDelete: (id) => {
             App.tasks.splice(id, 1)
             localStorage.setItem("memory", JSON.stringify(App.tasks))
+            App.tasksDones.splice(id, 1)
+            localStorage.setItem("done", JSON.stringify(App.tasksDones))
         },
         taskUp: (id) => {
             if (id != 0) {
-                let myArr = JSON.parse(localStorage.getItem("memory"))
-                let upItem = myArr[id];
-                let downItem = myArr[id - 1];
-                myArr[id - 1] = upItem;
-                myArr[id] = downItem;
-                localStorage.setItem("memory", JSON.stringify(myArr));
-                App.tasks = myArr
+                let myArrText = JSON.parse(localStorage.getItem("memory"));
+                let myArrDone = JSON.parse(localStorage.getItem("done"));
+                let upItem = myArrText[id];
+                let downItem = myArrText[id - 1];
+                let upItemDone = myArrDone[id];
+                let downItemDone = myArrDone[id - 1];
+                myArrText[id - 1] = upItem;
+                myArrText[id] = downItem;
+                myArrDone[id - 1] = upItemDone;
+                myArrDone[id] = downItemDone;
+                localStorage.setItem("memory", JSON.stringify(myArrText));
+                localStorage.setItem("done", JSON.stringify(myArrDone));
+                App.tasks = myArrText;
+                App.tasksDones = myArrDone;
+
             }
         },
         taskDown: (id) => {
-            let myArr = JSON.parse(localStorage.getItem("memory"))
-            let upItem = myArr[id];
-            let downItem = myArr[id + 1];
-            myArr[id + 1] = upItem;
-            myArr[id] = downItem;
-            localStorage.setItem("memory", JSON.stringify(myArr));
-            App.tasks = myArr
+
+            let myArrText = JSON.parse(localStorage.getItem("memory"));
+            let myArrDone = JSON.parse(localStorage.getItem("done"));
+            let upItem = myArrText[id];
+            let downItem = myArrText[id + 1];
+            let upItemDone = myArrDone[id];
+            let downItemDone = myArrDone[id + 1];
+            myArrText[id + 1] = upItem;
+            myArrText[id] = downItem;
+            myArrDone[id + 1] = upItemDone;
+            myArrDone[id] = downItemDone;
+            localStorage.setItem("memory", JSON.stringify(myArrText));
+            localStorage.setItem("done", JSON.stringify(myArrDone));
+            App.tasks = myArrText;
+            App.tasksDones = myArrDone;
         },
         test: () => {
             console.log("test-TAB");
             App.tasks = ["Посрать", "Вытереть жопу", "Помыть руки", "Позвонить Андрюше", "Сходить в гудвин", "Попить кофе"]
         },
 
-        taskDone: (target) => {
-            console.log(target.path[2])
+        taskDone: (id) => {
+            if (App.tasksDones[id] == App.icons[0]) {
+                App.tasksDones.splice(id, 1, App.icons[1])
+                localStorage.setItem("done", JSON.stringify(App.tasksDones))
+            } else {
+                App.tasksDones.splice(id, 1, App.icons[0])
+                localStorage.setItem("done", JSON.stringify(App.tasksDones))
+            }
+
+        },
+        taskDoneRander: (id) => {
+
+            if (App.tasksDones[id] == App.icons[1]) {
+                console.log(App.tasksDones[id]);
+                App.checked = "checked";
+                // console.log(checked)
+                // console.log(App.checked);
+            } else if (App.tasksDones[id] == App.icons[0]) {
+                console.log(App.tasksDones[id]);
+                App.checked = "";
+                // console.log(checked)
+                // console.log(App.checked);
+            }
         }
+
     }
 })
